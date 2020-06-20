56a40738fb53d39231b7b8b3bc6a106f2b33fc8f
Richard R. Chang <65420925+NYRChang@users.noreply.github.com>
2020-06-20 14:50:28 -0400

    write to csv file path
    file path

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,7 +1,10 @@
 # app/robo_advisor.py
 
-import requests
 import json
+import csv
+import os
+
+import requests
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(request_url)
@@ -32,12 +35,18 @@ latest_close = tsd[latest_day]["4. close"]
 
 #get high price of each day
 high_prices = []
+low_prices = []
 for date in dates:
     high_price = float(tsd[date]["2. high"])
     high_prices.append(high_price)
+    low_price = float(tsd[date]["3. low"])
+    low_prices.append(low_price)
 
-#maximum of all the high prices
+#max/min of all the high prices
 recent_high = max(high_prices)
+recent_low = min(low_prices)
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
 
 print("-------------------------")
@@ -49,10 +58,22 @@ print("-------------------------")
 print(f"LATEST DAY: {latest_day}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}")
+print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
+
+
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
